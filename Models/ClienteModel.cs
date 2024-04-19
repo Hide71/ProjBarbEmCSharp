69b40508ee94d1652a3fe8a112d0888a08e1e044
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2024-04-19 13:17:31 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ClienteModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjBarb.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Documento { get; set; }
+    }
+}
